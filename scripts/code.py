fc1e52dff6bed67101ed0d94d48bb887a2d0e087
YaaraPer <yaaraperlmuter@gmail.com>
2026-03-02 18:28:52 +0200

    כתיבת התוצאות בקובץ וסגירותו.

--- scripts/code.py
+++ scripts/code.py
@@ -12,7 +12,6 @@ sickle_cell_freq.write("Generation\tFreq_S\tFreq_SS\tFreq_AS\tFreq_AA\n")
 for i in range(1, num_gen+1):
     
     p_A = 1 - q_S
-
     AS = 2*p_A*q_S
     AA = p_A**2
     SS = q_S**2
@@ -20,4 +19,7 @@ for i in range(1, num_gen+1):
     # חישוב שכיחות האלל HbS בדור הבא
     q_S = q_S / (0.98*p_A + 2*q_S)
 
+    sickle_cell_freq.write(f"{i}\t{round(q_S,5)}\t{round(SS,5)}\t{round(AS,5)}\t{round(AA,5)}\n")
+
+sickle_cell_freq.close()
     
